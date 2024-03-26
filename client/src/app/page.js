4bef52d450229c6b241ff13d00430175cbf5aaaf
rishabhraghwendra@18 <rishabhraghwendra2002@gmail.com>
2024-03-26 17:00:15 +0530

    feed section ready

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,10 +1,25 @@
+'use client'
+import { useState } from "react";
 import Image from "next/image";
 import styles from "./page.module.css";
+import InstagramTextLogo from "../assets/instagram-text.png";
+import Button from "@/components/Button/Button";
+import DisplayCard from "@/components/DisplayCard/DisplayCard";
+
+
 
 export default function Home() {
   return (
     <>
-      home page
+      <div className={styles.navbar}>
+        <Image src={InstagramTextLogo} width={200} height={'auto'} alt="Instagram logo"/>
+      </div>
+      <div className={styles.main}>
+      <DisplayCard/>
+      </div>
+      <div className={styles.footer}>
+        <Button className={styles.button}>Create a Post</Button>
+      </div>
     </>
   );
 }
---
+++ client/src/components/DisplayCard/DisplayCard.js
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+export default function DisplayCard() {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Shrimp and Chorizo Paella"
+        subheader="September 14, 2016"
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image="/static/images/cards/paella.jpg"
+        alt="Paella dish"
+      />
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+      </CardActions>
+      <CardContent sx={{paddingTop:0}}>
+        <Typography variant="body2" color="text.secondary">
+          (User here) Perfect Caption
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+      <Typography variant="body2" color="text.secondary">
+          See comments..
+        </Typography>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>
+            (username) Comment 1
+          </Typography>
+          <Typography paragraph>
+          (username) Comment 2
+          </Typography>
+          <Typography paragraph>
+          (username) Comment 3
+          </Typography>
+          <input placeholder="add comment"></input>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
